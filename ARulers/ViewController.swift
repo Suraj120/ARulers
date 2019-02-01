73253b6dfa292fd94b32e3749b9f607cf6fce6b8
Teja Swaroop <TejaSwaroop@01hw988609.local>
2019-02-01 15:26:12 +0530

    Area and distance code

--- ARulers/ViewController.swift
+++ ARulers/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     var dotNodes = [SCNNode]()
     var textNode = SCNNode()
+    var allX = [Double]()
+    var allY = [Double]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,13 +49,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         print("number of nodes \(dotNodes.count)")
         
-//        if dotNodes.count >= 5 {
-//            for dot in dotNodes {
-//                dot.removeFromParentNode()
-//            }
-//            dotNodes = [SCNNode]()
-//        }
-        
+        if dotNodes.count >= 4 {
+            for dot in dotNodes {
+                //dot.removeFromParentNode()
+            }
+            //dotNodes = [SCNNode]()
+        }
         if let touchLocation = touches.first?.location(in: sceneView) {
             
             let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
@@ -76,60 +77,123 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let dotNode = SCNNode(geometry: dotGeometry)
         dotNode.position = SCNVector3Make(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
         
-        //dotNode.position = SCNVector3Make(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, 0.0)
-        
-        
-        
         sceneView.scene.rootNode.addChildNode(dotNode)
         
         dotNodes.append(dotNode)
-        
-        if dotNodes.count >= 4 {
-            calculateCoordinates()
+        calculateCoordinates()
+        if dotNodes.count == 2 {
+            calculate()
+            
+        }else  if dotNodes.count >= 3 {
+            poygonArea(X: allX, Y: allY, points: dotNodes.count)
         }
+        drawLines()
+        
     }
     
-    var allX = [Float]()
-    var allY = [Float]()
+    
+    
     func calculateCoordinates() {
-        
+        allX.removeAll()
+        allY.removeAll()
         for val in dotNodes {
             
-            allY.append(val.position.y)
-            allX.append(val.position.x)
-            print("coordinates\(val)")
+            allY.append(Double(val.position.y))
+            allX.append(Double(val.position.x))
     }
+         print("x values \(allX)")
         print("y values \(allY)")
-        print("x values \(allX)")
+        
+       
         
     }
     
     
-//    func calculate (){
-//        let start = dotNodes[0]
-//        let end = dotNodes[1]
-//        //let end = dotNodes[dotNodes.count - 1]
-//
-//        for val in dotNodes.enumerated() {
-//            print("coordinates\(val)")
-//        }
-//
-//        print(start.position)
-//        print(end.position)
-//
-//        //distance in 3D space using pythogoras theorem!!!
-//
-//        let a = end.position.x - start.position.x
-//        let b = end.position.y - start.position.y
-//        let c = end.position.z - start.position.z
-//
-//        let distance = abs(sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2)))
-//        print("distance in space \(distance)")
-//
-//        updateText(text: "\(distance)", atPosition: end.position)
-//
-//
-//    }
+        func calculate (){
+            
+    let start = dotNodes[0]
+    let end = dotNodes[1]
+    
+    print(start.position)
+    print(end.position)
+    
+    //distance in 3D space using pythogoras theorem!!!
+    
+    let a = end.position.x - start.position.x
+    let b = end.position.y - start.position.y
+    let c = end.position.z - start.position.z
+    
+    let distance = abs(sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2)))
+    print("distance in space \(distance)")
+    
+    updateText(text: "\(distance)", atPosition: end.position)
+    
+    
+
+    
+   
+    
+}
+    
+    
+    
+    func drawLines() {
+        
+        for i in 0...dotNodes.count-1 {
+            
+            var lines : SCNNode = SCNNode()
+            if i == dotNodes.count-1 {
+                lines = line(startPoint: dotNodes[i].position, endPoint: dotNodes[0].position, color: UIColor.yellow)
+                
+            }
+            else {
+                lines = line(startPoint: dotNodes[i].position, endPoint: dotNodes[i+1].position, color: UIColor.yellow)
+            }
+            
+            sceneView.scene.rootNode.addChildNode(lines)
+            
+            
+        }
+        
+        
+        
+    }
+    
+    func line(startPoint: SCNVector3, endPoint: SCNVector3, color : UIColor) -> SCNNode
+    {
+        let vertices: [SCNVector3] = [startPoint, endPoint]
+        let data = NSData(bytes: vertices, length: MemoryLayout<SCNVector3>.size * vertices.count) as Data
+        
+        let vertexSource = SCNGeometrySource(data: data,
+                                             semantic: .vertex,
+                                             vectorCount: vertices.count,
+                                             usesFloatComponents: true,
+                                             componentsPerVector: 3,
+                                             bytesPerComponent: MemoryLayout<Float>.size,
+                                             dataOffset: 0,
+                                             dataStride: MemoryLayout<SCNVector3>.stride)
+        
+        
+        let indices: [Int32] = [ 0, 1]
+        
+        let indexData = NSData(bytes: indices, length: MemoryLayout<Int32>.size * indices.count) as Data
+        
+        let element = SCNGeometryElement(data: indexData,
+                                         primitiveType: .line,
+                                         primitiveCount: indices.count/2,
+                                         bytesPerIndex: MemoryLayout<Int32>.size)
+        
+        let line = SCNGeometry(sources: [vertexSource], elements: [element])
+        
+        line.firstMaterial?.lightingModel = SCNMaterial.LightingModel.constant
+        line.firstMaterial?.diffuse.contents = color
+        
+        let lineNode = SCNNode(geometry: line)
+        return lineNode;
+    }
+    
+    
+    
     
     func updateText(text: String, atPosition position:SCNVector3) {
         
@@ -144,27 +208,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
+    
+    
+    func poygonArea( X arrX:[Double], Y arrY:[Double], points totalPoints:Int) -> Double {
+        
+        let end = dotNodes[dotNodes.count-1]
+        var area = 0.0
+        var j = totalPoints-1
+        for val in 0..<totalPoints {
+            area = area + (arrX[j] + arrX[val]) * (arrY[j] - arrY[val])
+            j=val
+        }
+        print("area is \(area/2.0)")
+         updateText(text: "\(area/2)", atPosition:end.position )
+        
+        return area/2.0
 }
 
-//X, Y    Arrays of the x and y coordinates of the vertices, traced in a clockwise direction, starting at any vertex. If you trace them counterclockwise, the result will be correct but have a negative sign.
-//numPoints    The number of vertices
-//Returns    the area of the polygon
-
-
-
-//function polygonArea(X, Y, numPoints)
-//{
-//    area = 0;         // Accumulates area in the loop
-//    j = numPoints-1;  // The last vertex is the 'previous' one to the first
-//
-//    for (i=0; i<numPoints; i++)
-//    { area = area +  (X[j]+X[i]) * (Y[j]-Y[i]);
-//        j = i;  //j is previous vertex to i
-//    }
-//    return area/2;
-//}
-
-
+}
 
 
 
